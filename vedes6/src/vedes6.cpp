a1cab16aee1ff7199a88256ec3c7cc153e362a73
Szabó Gergely <gergely0116@freemail.hu>
2016-12-18 23:04:54 +0100

    camera 1.1

--- vedes6/src/vedes6.cpp
+++ vedes6/src/vedes6.cpp
@@ -25,9 +25,7 @@ void keyUp (unsigned char key, int x, int y) {
 	keyStates[key] = 0;
 }
 
-GLdouble alpha = PI/6;
-GLdouble beta = PI/6;
-GLdouble delta = 0.01;
+GLdouble delta = 0.0;
 
 POINT3D cam = POINT3D(5, 0, 0);
 
@@ -56,10 +54,6 @@ POINT3D WinMin = POINT3D(-1, -1, 0);
 
 POINT3D View1Max = POINT3D(600, 500, 0);
 POINT3D View1Min = POINT3D(200, 100, 0);
-
-
-
-
 
 void window_to_viewport(POINT3D wmax, POINT3D wmin, POINT3D vmax, POINT3D vmin, double WTV[][4]){
 
@@ -93,16 +87,10 @@ void display(){
 	POINT3D WxU = vectorial_product(W, U);
 	POINT3D V = vect_norm(WxU);
 
-	//std::cout << WxU.x << " " << WxU.y << " " << WxU.z << std::endl;
-	//std::cout << norm(KP) << std::endl;
-
 	GLdouble K[4][4] = {{U.x, U.y, U.z, -(inner_product(cam, U))}, {V.x, V.y, V.z, -(inner_product(cam, V))}, {W.x, W.y, W.z, -(inner_product(cam, W))}, {0, 0, 0, 1}};
-	//GLdouble K[4][4] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	GLdouble WTV[4][4] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0},{0, 0, 0}};
 
 	window_to_viewport(WinMax, WinMin, View1Max, View1Min, WTV);
-
-	GLdouble Y[4][4] = {{cos(alpha), 0, sin(alpha), 0},{0, 1, 0, 0}, {-sin(alpha), 0, cos(alpha), 0}, {0, 0, 0, 1}};
 
 	GLdouble Vc[4][4] = {{1, 0, 0, 0},{0, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, -1.0/3, 1}};
 
@@ -113,26 +101,35 @@ void display(){
 
 	keyOperations();
 
-	/*mul_matrices(K, Y, temp1);
-	mul_matrices(Vc, temp1, temp2);
-	mul_matrices(WTV, temp2, M);*/
-
 	mul_matrices(Vc, K, temp1);
 	mul_matrices(WTV, temp1, M);
 
-	for(int i = 0; i < 4; i++){
+	/*for(int i = 0; i < 4; i++){
 		for(int j = 0; j < 4; j++){
 			std::cout << "K[" << i << "][" << j << "] = " << K[i][j] << std::endl; 
 		}
-	}
+	}*/
 
 	Cube A1 = Cube();
 	A1.transformPoints(M);
 	A1.draw();
+
 	Cube A2 = Cube(0, 0, 2);
+	A2.transformPoints(M);
+	A2.draw();
+
 	Cube A3 = Cube(0, 0, -2);
+	A3.transformPoints(M);
+	A3.draw();
+
 	Cube A4 = Cube(0, 2, 0);
+	A4.transformPoints(M);
+	A4.draw();
+
 	Cube A5 = Cube(0, -2, 0);
+	A5.transformPoints(M);
+	A5.draw();
+
 	Cube A6 = Cube(2, 0, 0);
 	A6.transformPoints(M);
 	A6.draw();
@@ -140,18 +137,6 @@ void display(){
 	Cube A7 = Cube(-2, 0, 0);
 	A7.transformPoints(M);
 	A7.draw();
-
-	A2.transformPoints(M);
-	A3.transformPoints(M);
-	A4.transformPoints(M);
-	A5.transformPoints(M);
-	
-	A2.draw();
-	A3.draw();
-	A4.draw();
-	A5.draw();
-	
-	
 
 	glutSwapBuffers();
 
